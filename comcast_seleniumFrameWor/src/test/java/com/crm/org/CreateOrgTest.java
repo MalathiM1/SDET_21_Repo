c65eea30ccb7c9c0a90692f862e6f39eb58273c4
CLIENT <CLIENT@CLIENT-PC>
2021-09-23 12:14:44 -0400

    push

---
+++ comcast_seleniumFrameWor/src/test/java/com/crm/org/CreateOrgTest.java
@@ -0,0 +1,27 @@
+package com.crm.org;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test(groups="Smoke")
+	public void createorganization()
+	
+	{
+		System.out.println("created organization ");
+	}
+	
+	@Test(groups="regression")
+	public void modifiedorganization()
+	
+	{
+		System.out.println("modified organization ");
+	}
+	@Test(groups="regression")
+	public void deleteorganization()
+	
+	{
+		System.out.println("deleted organization ");
+	}
+
+
+}
